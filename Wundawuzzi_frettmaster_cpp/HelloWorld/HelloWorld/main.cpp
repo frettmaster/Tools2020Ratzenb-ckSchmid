49d02f97bebac65da4aceceb7ffa971f78298a30
Wundawuzzi <tristanuhd@outlook.at>
2020-05-07 17:02:04 +0200

    normally

--- Wundawuzzi_frettmaster_cpp/HelloWorld/HelloWorld/main.cpp
+++ Wundawuzzi_frettmaster_cpp/HelloWorld/HelloWorld/main.cpp
@@ -8,6 +8,8 @@ void thisIsANewMethod()
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Hello world!" << std::endl;	
+	std::cout << "Hello world!" << std::endl;
+	std::cout << "Hallo Welt!" << std::endl;
+	std::cout << "Guten Tag!" << std::endl;
 	return 0;
 }
