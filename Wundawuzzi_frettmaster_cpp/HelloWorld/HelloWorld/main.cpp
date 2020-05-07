51c397f977d9521efc0e23fe1895b17e81691900
Wundawuzzi <tristanuhd@outlook.at>
2020-05-07 17:04:03 +0200

    normally

--- Wundawuzzi_frettmaster_cpp/HelloWorld/HelloWorld/main.cpp
+++ Wundawuzzi_frettmaster_cpp/HelloWorld/HelloWorld/main.cpp
@@ -11,5 +11,6 @@ int main(int argc, char* argv[])
 	std::cout << "Hello world!" << std::endl;
 	std::cout << "Hallo Welt!" << std::endl;
 	std::cout << "Guten Tag!" << std::endl;
+	std::cout << "noch eine neue Zeile" << std::endl;
 	return 0;
 }
